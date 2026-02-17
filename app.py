b2a90894d8f6ee27774d2d2df03da414ec31cdc7
igukusamuel <32755662+igukusamuel@users.noreply.github.com>
2026-02-16 21:49:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,13 @@
 import streamlit as st
-from ui.login import login_screen
-from ui.shift_ui import shift_open_screen
 from ui.pos_ui import pos_screen
-from ui.close_shift_ui import close_shift_screen
+from ui.admin_dashboard import admin_dashboard
 
-st.set_page_config(layout="wide")
+st.set_page_config(page_title="All-in-One POS & Admin", layout="wide")
 
-if "user" not in st.session_state:
-    st.session_state.user = None
+st.sidebar.title("Navigation")
+app_mode = st.sidebar.radio("Go to", ["POS", "Admin Dashboard"])
 
-if "shift_open" not in st.session_state:
-    st.session_state.shift_open = False
-
-if st.session_state.user is None:
-    login_screen()
-elif not st.session_state.shift_open:
-    shift_open_screen()
-else:
+if app_mode == "POS":
     pos_screen()
-
+else:
+    admin_dashboard()
