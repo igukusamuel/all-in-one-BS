75f781b39d7e491a75ef17aba82f67a075986a02
igukusamuel <32755662+igukusamuel@users.noreply.github.com>
2026-02-16 22:35:41 -0500

    Update shift_service.py

--- services/shift_service.py
+++ services/shift_service.py
@@ -1,1 +1,12 @@
+def open_shift(float_amount):
+    return {
+        "is_open": True,
+        "beginning_cash": float_amount,
+        "cash_sales": 0
+    }
+
+def close_shift(shift_data, actual_cash):
+    expected = shift_data["beginning_cash"] + shift_data["cash_sales"]
+    variance = actual_cash - expected
+    return expected, variance
 
