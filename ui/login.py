570052808907525807db2cdd9287ef4a6e037ec4
igukusamuel <32755662+igukusamuel@users.noreply.github.com>
2026-02-16 19:48:34 -0500

    Update login.py

--- ui/login.py
+++ ui/login.py
@@ -1,1 +1,11 @@
+import streamlit as st
+
+def login_screen():
+    st.title("Retail OS - Login")
+
+    username = st.text_input("Username")
+    if st.button("Login"):
+        if username:
+            st.session_state.user = username
+            st.rerun()
 
