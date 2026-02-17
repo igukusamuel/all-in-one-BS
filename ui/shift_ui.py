2a0b1e4ac840763de6c650105e4c49f539d39293
igukusamuel <32755662+igukusamuel@users.noreply.github.com>
2026-02-16 19:48:57 -0500

    Update shift_ui.py

--- ui/shift_ui.py
+++ ui/shift_ui.py
@@ -1,1 +1,13 @@
+import streamlit as st
+from core.shift import open_shift
+
+def shift_open_screen():
+    st.title("Open Shift")
+
+    float_amount = st.number_input("Enter Opening Float", min_value=0.0)
+
+    if st.button("Start Shift"):
+        open_shift(float_amount)
+        st.session_state.shift_open = True
+        st.rerun()
 
