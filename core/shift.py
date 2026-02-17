a81516cf92bbb14af6106a45d8141a8cc27b3529
igukusamuel <32755662+igukusamuel@users.noreply.github.com>
2026-02-16 19:49:26 -0500

    Update shift.py

--- core/shift.py
+++ core/shift.py
@@ -1,1 +1,10 @@
+import streamlit as st
+import datetime
 
+def open_shift(float_amount):
+    st.session_state.shift = {
+        "shift_id": datetime.datetime.now().strftime("%Y%m%d%H%M%S"),
+        "float": float_amount,
+        "cash_sales": 0,
+        "cash_counted": 0
+    }
