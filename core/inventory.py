cf2a5a79bd1b2621aedea154b60322f25d254a08
igukusamuel <32755662+igukusamuel@users.noreply.github.com>
2026-02-16 19:50:16 -0500

    Update inventory.py

--- core/inventory.py
+++ core/inventory.py
@@ -1,1 +1,14 @@
+import streamlit as st
+
+def get_inventory():
+    if "inventory" not in st.session_state:
+        st.session_state.inventory = [
+            {"name": "Coffee", "price": 10, "cost": 5, "stock": 1000}
+        ]
+    return st.session_state.inventory
+
+def reduce_inventory(name, qty):
+    for item in st.session_state.inventory:
+        if item["name"] == name:
+            item["stock"] -= qty
 
