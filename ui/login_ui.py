447b1e0b3cd96c0ac01562e3b62620573b299285
igukusamuel <32755662+igukusamuel@users.noreply.github.com>
2026-02-16 22:36:07 -0500

    Update login_ui.py

--- ui/login_ui.py
+++ ui/login_ui.py
@@ -1,1 +1,17 @@
+import streamlit as st
+from core.auth import authenticate
+
+def login_screen():
+    st.title("Login")
+
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+
+    if st.button("Login"):
+        user = authenticate(username, password)
+        if user:
+            st.session_state.user = user
+            st.rerun()
+        else:
+            st.error("Invalid credentials")
 
